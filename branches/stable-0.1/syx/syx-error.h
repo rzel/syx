a85ae82b343b17871f24cd1dbbb12d03cb342005
lethalman88 <lethalman88@b2e5c414-8c31-0410-8979-97a32ceef242>
2007-07-05 19:42:57 +0000

    Branched stable 0.1.1

---
+++ branches/stable-0.1/syx/syx-error.h
@@ -0,0 +1,82 @@
+/* 
+   Copyright (c) 2007 Luca Bruno
+
+   This file is part of Smalltalk YX.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER    
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef SYX_ERROR_H
+#define SYX_ERROR_H
+
+#include <stdio.h>
+#include "syx-types.h"
+#include "syx-object.h"
+
+typedef syx_uint32 SyxErrorType;
+
+typedef struct SyxErrorEntry SyxErrorEntry;
+
+struct SyxErrorEntry
+{
+  syx_symbol name;
+  SyxOop class;
+};
+
+void syx_error_init (void);
+void syx_error_clear (void);
+SyxErrorType syx_error_register (syx_symbol name, SyxOop class);
+SyxErrorEntry *syx_error_lookup (SyxErrorType type);
+
+#define syx_signal(type, args...)					\
+  syx_interp_enter_context (syx_send_message (syx_interp_get_current_context (), \
+					      syx_error_lookup (type)->class, \
+					      "signal",			\
+					      args))
+
+#define syx_signal_does_not_understand(receiver, selector)		\
+  syx_interp_enter_context (syx_send_binary_message (syx_interp_get_current_context (), \
+						     receiver,		\
+						     "doesNotUnderstand:", \
+						     selector))
+
+#define syx_error(args...)			\
+  {						\
+    fprintf (stderr, "ERROR: ");		\
+    fprintf (stderr, args);			\
+    exit (EXIT_FAILURE);			\
+  }
+
+#define syx_warning(args...)			\
+  {						\
+    fprintf (stderr, "WARNING: ");		\
+    fprintf (stderr, args);			\
+  }
+
+#define syx_perror(args...)			\
+  {						\
+    perror (args);				\
+    exit (EXIT_FAILURE);			\
+  }
+
+#define syx_debug(args...)			\
+  printf (args)					
+    
+
+#endif /* SYX_ERROR_H */
---
+++ branches/stable-0.1/syx/syx-plugins.h
@@ -0,0 +1,54 @@
+/* 
+   Copyright (c) 2007 Luca Bruno
+
+   This file is part of Smalltalk YX.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell   
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER    
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef SYX_PLUGINS_H
+#define SYX_PLUGINS_H
+
+#include "syx-interp.h"
+
+typedef struct SyxPluginEntry SyxPluginEntry;
+
+struct SyxPluginEntry
+{
+  syx_string name;
+  syx_pointer handle;
+};
+
+/* Dealing with dynamic libraries */
+
+syx_pointer syx_library_open (syx_symbol location);
+syx_pointer syx_library_symbol (syx_pointer handle, syx_symbol name);
+syx_bool syx_library_close (syx_pointer handle);
+
+/* Managing plugins */
+
+typedef syx_bool (* SyxPluginInitializeFunc) (void);
+typedef void (* SyxPluginFinalizeFunc) (void);
+
+void syx_plugin_finalize (void);
+syx_pointer syx_plugin_load (syx_symbol name);
+syx_bool syx_plugin_unload (syx_symbol name);
+syx_bool syx_plugin_call (SyxExecState *es, SyxOop method);
+
+#endif /* SYX_PLUGINS_H */
