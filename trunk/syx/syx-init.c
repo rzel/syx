524db7672c8b08a3a14dbbda3d0a73d83020fb42
lethalman88 <lethalman88@b2e5c414-8c31-0410-8979-97a32ceef242>
2007-07-14 09:40:19 +0000

    Added Fraction and a basic division for small integers. Fixed handling of minus character in lexer

--- trunk/syx/syx-init.c
+++ trunk/syx/syx-init.c
@@ -84,7 +84,7 @@ _syx_file_in_basic (void)
   static syx_symbol kernel_filenames[] = {
     "Behavior.st", "Metaclass.st",
     "Symbol.st",
-    "Magnitude.st", "Number.st", "Integer.st", "SmallInteger.st", "LargeInteger.st", "Float.st",
+    "Magnitude.st", "Number.st", "Integer.st", "SmallInteger.st", "LargeInteger.st", "Fraction.st", "Float.st",
     "Object.st", "UndefinedObject.st", "ObjectMemory.st",
     "Collection.st", "ArrayedCollection.st", "SequenceableCollection.st", "OrderedCollection.st",
     "Character.st", "ByteArray.st", "String.st",
