425613e13007e742bcfa14f2a619113fadddecee
lethalman88 <lethalman88@b2e5c414-8c31-0410-8979-97a32ceef242>
2008-01-15 18:58:59 +0000

    Basic Date class has been added

--- trunk/syx/syx-init.c
+++ trunk/syx/syx-init.c
@@ -129,7 +129,7 @@ _syx_file_in_basic (void)
     "Interval.st",
     "Set.st", "Bag.st",
     "Dictionary.st", "IdentityDictionary.st", "SystemDictionary.st",
-    "DateTime.st",
+    "Date.st", "DateTime.st",
     "Compiler.st",
     "MVC.st",
     "CommandLine.st", "Console.st",
